38d0096d0718afa75ff71c687d37abd68a7c6cf7
Henrique Morbin <morbin_@hotmail.com>
2019-03-26 13:02:05 -0300

    Initial Commit

---
+++ MemeGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeGenerator
+//
+//  Created by Henrique Morbin on 26/03/19.
+//  Copyright © 2019 Henrique Morbin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
